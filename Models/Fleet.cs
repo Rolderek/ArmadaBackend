735959807128e4461073d1195a66e92435058ece
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-08-10 14:08:21 +0200

    Fleet.cs-be bele írva a teszt flotta string

--- Models/Fleet.cs
+++ Models/Fleet.cs
@@ -7,4 +7,31 @@
         public List<Ship> Ships { get; set; } = new List<Ship>();
         public List<SquadronElement> Squadrons { get; set; } = new List<SquadronElement>();
     }
+
+    /*
+     * Id | FleetName |
+     * one ship 000 + cards, one card 000 char - sum(6 ship with 6 cards) 108 char |
+     * one squadron 000 - 60 char |
+     * objectives sum(3*2), 6 char
+     * total 108+57+6 = 171 char
+    */
+
+    //ai tanácsa:
+    /*
+     F1#S:3[1,30,17,20];5[35,2,7]#Q:10*3;35;42#O:A=101;D=102;N=103
+     Jelentése:
+     F1: formátumverzió;
+     S: hajók;
+     3[...]: a 3 hajókártya-verzió, a zárójelben az upgrade-verziók;
+     ;: következő hajó;
+     Q: squadronok;
+     10*3: a 10-es squadronból három darab;
+     35 és 42: egy-egy squadron;
+     O: objective-ek;
+     A, D, N: Assault, Defense és Navigation.
+     
+    Összefoglalva:
+    400 pontos flotta: 5000 karakter
+    4000 pontos flotta: 50 000 karakter
+    */
 }
