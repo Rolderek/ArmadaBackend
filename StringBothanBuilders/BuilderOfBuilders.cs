f628b76a720b5aa433446c1cc84bf3ceca6e1ad0
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-08-11 08:47:32 +0200

    folytatva

--- StringBothanBuilders/BuilderOfBuilders.cs
+++ StringBothanBuilders/BuilderOfBuilders.cs
@@ -1,9 +1,26 @@
 ﻿using ArmadaBackend.Models;
 
 namespace ArmadaBackend.StringBotanBuilders
+    //Ez fogja a végleges szöveget összerakni, az elválasztókkal és itt lehet majd verziót állítani, írni.
+    //F1#TesztNév#S:3[1,30,17,20];5[35,2,7]#Q:10*3;35;42#O:A=10;D=40;N=99;
 {
     public class BuilderOfBuilders
     {
+        public string Starter { get; } = "F1";
+        public string FleetString { get; set; } = string.Empty;
+
+        public void MakingFleetStringV1(string fleetName, string shipString, string squadronString, string objectiveString)
+        {
+            FleetString = $"{Starter}#{fleetName}#{shipString}#{squadronString}#{objectiveString}";
+        }
+
+
+
+
+        public bool IsThereAnyStringValue()
+        {
+            return FleetString is null ? false : true;
+        }
     }
 
     // F1#TesztNév#S:3[1,30,17,20];5[35,2,7]#Q:10*3;35;42#O:A=101;D=102;N=103;
